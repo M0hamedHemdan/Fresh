aaff12c8a93635ad32e197f32db994dc04d10cf9
M0hamedHemdan <mhamedhemdan0@gmail.com>
2025-02-12 00:31:26 +0200

    imge

--- src/Composents/Orderdetails/Orderdetails.jsx
+++ src/Composents/Orderdetails/Orderdetails.jsx
@@ -96,7 +96,7 @@ export default function Orderdetails() {
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-16 py-3">
-                <span className="sr-only">Image</span>
+                <span className="">Image</span>
               </th>
               <th scope="col" className="hidden md:table-header-group px-6 py-3">
                 Product
